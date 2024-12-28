96614c345978ad2e989d7b50837ebb6651db4713
Lê Thanh Lâm <81215603+thanhlam1909@users.noreply.github.com>
2024-12-29 00:20:45 +0700

    Cập nhật lại file css Thêm controller ProductDetail

---
+++ 21BITV03_Nhom04_website_clothes/Controllers/ProductDetailController.cs
@@ -0,0 +1,18 @@
+﻿using _21BITV03_Nhom04_website_clothes.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _21BITV03_Nhom04_website_clothes.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        private readonly WebsiteClothesContext _context;
+        public ProductDetailController(WebsiteClothesContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
